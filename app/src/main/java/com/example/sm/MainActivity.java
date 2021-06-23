a7e16a898364dc0d3873bc335029ce906da3621b
ilirjanaqelaj <63592509+ilirjanaqelaj@users.noreply.github.com>
2021-06-24 00:57:42 +0200

    Lidhja me Login, dhe krijimi i animacionit.

--- app/src/main/java/com/example/sm/MainActivity.java
+++ app/src/main/java/com/example/sm/MainActivity.java
@@ -5,37 +5,51 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
 
-    public class MainActivity extends AppCompatActivity {
+import com.google.android.material.snackbar.Snackbar;
 
-        Button mRegisterBtn,mLoginBtn;
+public class MainActivity extends AppCompatActivity {
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
+    Button mRegisterBtn, mLoginBtn;
+    ImageView logo;
 
-            mRegisterBtn= findViewById(R.id.register_btn);
-            mLoginBtn= findViewById(R.id.login_btn);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mRegisterBtn = findViewById(R.id.register_btn);
+        mLoginBtn = findViewById(R.id.login_btn);
+        logo = findViewById(R.id.logoid);
+
+        logo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Animation animation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.bounce);
+                logo.startAnimation(animation);
+            }
+        });
 
 
-            mRegisterBtn.setOnClickListener(new View.OnClickListener(){
-                @Override
-                public void onClick(View v)
-                {
-                    startActivity(new Intent(MainActivity.this,RegisterActivity.class));
-                }
+        mRegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+            }
 
-            });
+        });
 
-            mLoginBtn.setOnClickListener(new View.OnClickListener(){
-                @Override
-                public void onClick(View v)
-                {
-                    startActivity(new Intent( MainActivity.this, LoginActivity.class));
-                }
-            });
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
 
-        }
+
     }
+}
