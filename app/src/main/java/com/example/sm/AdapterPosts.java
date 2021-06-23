0baeab6dd3889a48ae63fab6b5034535c15c93db
JetliraFetahaj <59027249+JetliraFetahaj@users.noreply.github.com>
2021-06-24 01:09:54 +0200

    Adapter post activity.

---
+++ app/src/main/java/com/example/sm/AdapterPosts.java
@@ -0,0 +1,86 @@
+package com.example.sm;
+
+import android.content.Context;
+import android.text.format.DateFormat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CalendarView;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
+
+public class AdapterPosts extends RecyclerView.Adapter<AdapterPosts.MyHolder> {
+
+    Context context;
+    List<ModelPost> postList;
+
+    public AdapterPosts(Context context, List<ModelPost> postList) {
+        this.context = context;
+        this.postList = postList;
+    }
+
+
+    @NonNull
+    @Override
+    public MyHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.row_posts, viewGroup, false);
+
+        return new MyHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyHolder myHolder, int i) {
+        String uid = postList.get(i).getUid();
+        String uEmail = postList.get(i).getuEmail();
+        String uName = postList.get(i).getuName();
+        String pId = postList.get(i).getpId();
+        String pTitle = postList.get(i).getpTitle();
+        String pDescription = postList.get(i).getpDescription();
+        String pTimeStamp = postList.get(i).getpTime();
+
+        Calendar calendar = Calendar.getInstance(Locale.getDefault());
+        calendar.setTimeInMillis(Long.parseLong(pTimeStamp));
+        String pTime = DateFormat.format("dd/MM/yyyy hh:mm aa", calendar).toString();
+
+        myHolder.uNameTv.setText(uName);
+        myHolder.pTimeTv.setText(pTime);
+        myHolder.pTitleTv.setText(pTitle);
+        myHolder.pDescriptionTv.setText(pDescription);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return postList.size();
+    }
+
+    class MyHolder extends RecyclerView.ViewHolder {
+
+        ImageView uPictureIv;
+        TextView uNameTv, pTimeTv, pTitleTv, pDescriptionTv;
+        ImageButton moreBtn;
+
+        public MyHolder(@NonNull View itemView) {
+            super(itemView);
+
+            uPictureIv = itemView.findViewById(R.id.uPictureIv);
+            uNameTv = itemView.findViewById(R.id.uNameTv);
+            pTimeTv = itemView.findViewById(R.id.pTimeTv);
+            pTitleTv = itemView.findViewById(R.id.pTitleTv);
+            pDescriptionTv = itemView.findViewById(R.id.pDescriptionTv);
+            moreBtn = itemView.findViewById(R.id.moreBtn);
+
+
+        }
+    }
+}
