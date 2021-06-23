9e94a0a78c7a98606ee019da56be1475ec4c92ee
ilirjanaqelaj <63592509+ilirjanaqelaj@users.noreply.github.com>
2021-06-24 01:00:02 +0200

    Implementimi i Recover Password dhe Remember me.

--- app/src/main/java/com/example/sm/LoginActivity.java
+++ app/src/main/java/com/example/sm/LoginActivity.java
@@ -1,11 +1,22 @@
 package com.example.sm;
-import android.app.ProgressDialog;
+
+import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.text.InputType;
 import android.util.Patterns;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,19 +24,36 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class LoginActivity extends AppCompatActivity {
+
     EditText mEmailEt, mPasswordEt;
-    TextView notHaveAccntTv;
+    TextView notHaveAccntTv, mRecoverPassTv;
     Button mLoginBtn;
+    SignInButton mGoogleLoginBtn;
     private FirebaseAuth mAuth;
-    //ProgressDialog pd;
+    RelativeLayout rel;
+    CheckBox mcheckBox;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+    Boolean savelogin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,15 +66,42 @@ public class LoginActivity extends AppCompatActivity {
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setDisplayShowHomeEnabled(true);
 
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+
         mAuth = FirebaseAuth.getInstance();
 
         mEmailEt = findViewById(R.id.emailEt);
         mPasswordEt = findViewById(R.id.passwordEt);
         notHaveAccntTv = findViewById(R.id.notHaveAccntTv);
+        mRecoverPassTv = findViewById(R.id.recoverPassTv);
         mLoginBtn = findViewById(R.id.login_btn);
+        rel = findViewById(R.id.relID);
+        mcheckBox = findViewById(R.id.checkbox);
+
+        sharedPreferences = getSharedPreferences("loginref", MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+        savelogin = sharedPreferences.getBoolean("savelogin", true);
+        if (savelogin == true) {
+            mEmailEt.setText(sharedPreferences.getString("email", null));
+            mPasswordEt.setText(sharedPreferences.getString("password", null));
+        }
+
+
+        mRecoverPassTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showRecoverPasswordDialog();
+            }
+        });
 
 
     }
+
+
     @Override
     public boolean onSupportNavigateUp() {
         onBackPressed();
@@ -60,9 +115,17 @@ public class LoginActivity extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
+
                             //pd.dismiss();
                             FirebaseUser user = mAuth.getCurrentUser();
-                            startActivity(new Intent(LoginActivity.this, ProfileActivity.class));
+                            if (mcheckBox.isChecked()) {
+                                editor.putBoolean("savelogin", true);
+                                editor.putString("email", email);
+                                editor.putString("password", passw);
+                                editor.commit();
+                            }
+
+                            startActivity(new Intent(LoginActivity.this, DashboardActivity.class));
                             finish();
                         } else {
                             //pd.dismiss();
@@ -84,21 +147,114 @@ public class LoginActivity extends AppCompatActivity {
     public void onLoginButton(View view) {
         String email = mEmailEt.getText().toString();
         String passw = mPasswordEt.getText().toString();
-       // if(email.contains("@") && passw.length()>5)
-        if(Patterns.EMAIL_ADDRESS.matcher(email).matches())
-        {
+
+        if (Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
             loginUser(email, passw);
-        }
-        else{
-            Toast.makeText(this,  "Email or password wrong!", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Email or password wrong!", Toast.LENGTH_SHORT).show();
         }
     }
-
-
 
 
     public void onNotHaveText(View view) {
-        startActivity(new Intent( LoginActivity.this, RegisterActivity.class));
+        startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
 
     }
+
+    private void showRecoverPasswordDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Recover Password");
+
+        LinearLayout linearLayout = new LinearLayout(this);
+        EditText emailEt = new EditText(this);
+        emailEt.setHint("Email");
+        emailEt.setInputType(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
+        emailEt.setMinEms(16);
+
+        linearLayout.addView(emailEt);
+        linearLayout.setPadding(10, 10, 10, 10);
+        builder.setView(linearLayout);
+
+
+        builder.setPositiveButton("Recover", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String email = emailEt.getText().toString();
+                beginRecovery(email);
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        builder.create().show();
+
+    }
+
+    private void beginRecovery(String email) {
+        mAuth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    Snackbar snackbar = Snackbar.make(rel, "Email sent", Snackbar.LENGTH_LONG);
+                    snackbar.show();
+                    //  Toast.makeText(LoginActivity.this,"Email sent",Toast.LENGTH_SHORT).show();
+                } else
+                    Toast.makeText(LoginActivity.this, "Failed...", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(LoginActivity.this, "" + e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+  /*  private void firebaseAuthWithGoogle(String idToken) {
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                            String email = user.getEmail();
+                            String uid = user.getUid();
+                            HashMap<Object, String> hashMap = new HashMap<>();
+                            hashMap.put("email", email);
+                            hashMap.put("uid", uid);
+                            hashMap.put("name", "");
+                            hashMap.put("phone", "");
+                            hashMap.put("image", "");
+
+                            FirebaseDatabase database = FirebaseDatabase.getInstance();
+                            DatabaseReference reference = database.getReference("Users");
+                            reference.child(uid).setValue(hashMap);
+
+
+                            Toast.makeText(LoginActivity.this, "" + user.getEmail(), Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(LoginActivity.this, DashboardActivity.class));
+                        } else {
+
+                            Toast.makeText(LoginActivity.this, "Login Failed...", Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(LoginActivity.this, "" + e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+*/
+
+
 }
